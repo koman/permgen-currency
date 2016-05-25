c065ecd5c9339c6ed2bf4901233d60a2b50969bc
dev271 <koman.rudden@gpaa.gov.za>
2016-05-25 13:39:04 +0200

    Test case fixed

--- src/test/java/com/permgen/ApplicationTests.java
+++ src/test/java/com/permgen/ApplicationTests.java
@@ -1,32 +1,41 @@
 package com.permgen;
 
-import com.permgen.CurrencyApplication;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import static org.junit.Assert.assertEquals;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CurrencyApplication.class)
-@WebIntegrationTest(randomPort=true)
+@WebIntegrationTest(randomPort = true)
 public class ApplicationTests {
 
-	@Value("${local.server.port}")
-	private int port;
+    @Value("${currency_url}")
+    private String url;
 
-	private RestTemplate template = new TestRestTemplate();
+    private RestTemplate template;
 
-	@Test
-	public void resourceLoads() {
-		ResponseEntity<String> response = template.getForEntity("http://localhost:{port}/", String.class, port);
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-	}
+    @Before
+    public void startup() {
+        
+        SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.16.142.60", 3128));
+        clientHttpRequestFactory.setProxy(proxy);
+        template = new RestTemplate(clientHttpRequestFactory);        
+    }
+    
+    @Test
+    public void resourceLoads() {
+        
+        Currency response = template.getForObject(url + "USD", Currency.class);
+        assertEquals("USD", response.getBase());
+    }
 }
